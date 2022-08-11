cdc94c1b2eb0454183bc633d8efd310003f295d3
Pascal3366 <ppeinecke@protonmail.com>
2022-08-11 15:41:25 +0200

    Projektdateien hinzufügen.

---
+++ Project3/ItemVerwaltung.cs
@@ -0,0 +1,11 @@
+﻿namespace Project3
+{
+    public class ItemVerwaltung
+    {
+        public int IID { get; set; }
+        public string? Bezeichnung { get; set; }
+        public DateTime Von { get; set; }
+        public DateTime Bis { get; set; }
+        public bool Verfügbarkeit { get; set; }
+    }
+}
---
+++ Project3/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Project3
+{
+    public class User
+    {
+        public int UID { get; set; }
+        public string? Name { get; set; }
+        public string? Nachname { get; set; }
+    }
+}
